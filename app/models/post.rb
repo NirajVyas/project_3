2d7912d7011591da42211bccac66719d1dc262b7
NirajVyas <nirajvyas28@hotmail.com>
2014-06-09 16:16:36 +0100

    new post styling improved, youtube videos uploading but not when user inserts link

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :blog
-  attr_accessible :body, :date, :location, :stayed, :tags, :title, :photo_uploader
+  attr_accessible :body, :date, :location, :stayed, :tags, :title, :photo_uploader, :youtube_id
   mount_uploader :photo_uploader, PhotoUploaderUploader
 end
