1d4f9f0211ebca160a7dbe99ff92dac0760ddd8f
NirajVyas <nirajvyas28@hotmail.com>
2014-06-13 11:26:33 +0100

    favorite controller updated with create and destroy methods

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,26 @@
 class FavoritesController < ApplicationController
+
+  def create
+    user = current_user
+    @blog = Blog.find(params[:id])
+    user.mark_as_favorite [ @blog ]
+
+    respond_to do |format|
+        format.js {render '/blogs/favorite.js.erb'}
+        format.html { redirect_to :back }
+
+      end 
+  end
+
+  def destroy
+    user = current_user
+    @blog = Blog.find(params[:id])
+    user.remove_mark :favorite, [ @blog ]
+    respond_to do |format|
+        format.js {render '/blogs/favorite.js.erb'}
+        format.html { redirect_to :back }
+      end 
+  end
+
+
 end
