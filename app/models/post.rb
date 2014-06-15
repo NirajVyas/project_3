b67a7492ea45d96a7935cfda648500b1b196f9c6
NirajVyas <nirajvyas28@hotmail.com>
2014-06-15 11:21:10 +0100

    long and latitude added to Posts and updated model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments
 
   acts_as_commentable
-  attr_accessible :body, :date, :location, :stayed, :tags, :title, :photo_uploader, :video_attributes
+  attr_accessible :body, :date, :location, :stayed, :tags, :title, :photo_uploader, :video_attributes, :latitude, :longitude
   mount_uploader :photo_uploader, PhotoUploaderUploader
-
+  geocoded_by :location
   accepts_nested_attributes_for :video
 end
