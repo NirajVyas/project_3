608254c32fa3bc01404320c3f4c6d95f23b8b505
NirajVyas <nirajvyas28@hotmail.com>
2014-06-15 12:25:42 +0100

    uploading images to post not showing error but not displaying

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,10 @@ class Post < ActiveRecord::Base
   belongs_to :blog
   has_one :video
   has_many :comments
+  mount_uploader :blog_photo, BlogPhotoUploader
 
   acts_as_commentable
-  attr_accessible :body, :date, :location, :stayed, :tags, :title, :photo_uploader, :video_attributes, :latitude, :longitude
+  attr_accessible :body, :date, :location, :stayed, :tags, :title, :photo_uploader, :video_attributes, :latitude, :longitude, :blog_photo
   mount_uploader :photo_uploader, PhotoUploaderUploader
   geocoded_by :location
   after_validation :geocode
