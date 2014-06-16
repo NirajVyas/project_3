ffee6146fbc04938501537d938cc015797b1ace0
NirajVyas <nirajvyas28@hotmail.com>
2014-06-16 17:09:01 +0100

    gecode syntax update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,4 @@ class HomeController < ApplicationController
   end
 
 
-
-
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ActiveRecord::Base
   attr_accessible :city, :latitude, :longitude
   geocoded_by :city
-  after_validation :geocodem :if => :address_changed?
+  after_validation :geocode, :if => :address_changed?
 end
