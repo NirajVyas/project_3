236ca12cf97d813d1efc14d6463ab7348b478a44
NirajVyas <nirajvyas28@hotmail.com>
2014-06-22 21:37:55 +0100

    complete new layout added for nav bar and profiles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,31 +14,6 @@
 //= require jquery_ujs
 //= require jquery.purr
 //= require best_in_place
-//= require foundation
 //= require gmaps/google
 //= require underscore-min
-//= require lightbox.min
-//= require foundation.min
 //= require_tree .
-
-
-window.document.onkeydown = function (e)
-{
-    if (!e){
-        e = event;
-    }
-    if (e.keyCode == 27){
-        lightbox_close();
-    }
-}
-
-function lightbox_open(){
-    window.scrollTo(0,0);
-    document.getElementById('light').style.display='block';
-    document.getElementById('fade').style.display='block';  
-}
-
-function lightbox_close(){
-    document.getElementById('light').style.display='none';
-    document.getElementById('fade').style.display='none';
-}
